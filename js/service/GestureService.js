a1d13f96178de0b62c504663a2734e6edff49f93
Maxime ANDRE <maxime.andre@student.unamur.be>
2020-11-21 16:07:48 +0100

    Gif gestures

--- js/service/GestureService.js
+++ js/service/GestureService.js
@@ -39,9 +39,9 @@ class LeapMotionGestureService extends GestureService {
         this.controller.connect();
 
         this.recognizableGestures = new Map();
-        this.recognizableGestures.set("HAMMER", new GestureHammerLeapMotion("HAMMER", 3, "/assets/data/gestures-illustrations/hammer.png", []));
-        this.recognizableGestures.set("HAMMER_FAST", new GestureHammerLeapMotion("HAMMER_FAST", 2, "/assets/data/gestures-illustrations/hammer-fast.png", []));
-        this.recognizableGestures.set("ROTATION", new GestureRotationLeapMotion("ROTATION", 1.5, "/assets/data/gestures-illustrations/rotation.png", []));
+        this.recognizableGestures.set("HAMMER", new GestureHammerLeapMotion("HAMMER", 3, "/assets/data/gestures-illustrations/hammer.gif", []));
+        this.recognizableGestures.set("HAMMER_FAST", new GestureHammerLeapMotion("HAMMER_FAST", 2, "/assets/data/gestures-illustrations/hammer.gif", []));
+        this.recognizableGestures.set("ROTATION", new GestureRotationLeapMotion("ROTATION", 1.5, "/assets/data/gestures-illustrations/rotation.gif", []));
     }
 
     /**
