d5e48e00c9ae5c6866e5d9c4edb8a8d05ca612b2
Pierre Luycx <pierre.luycx@gmail.com>
2020-12-04 12:14:32 +0100

    Add reversed rotation GIF

--- js/service/GestureService.js
+++ js/service/GestureService.js
@@ -45,7 +45,7 @@ class LeapMotionGestureService extends GestureService {
         this.recognizableGestures.set("HAMMER", new GestureHammerLeapMotion("HAMMER", 3, "/assets/data/gestures-illustrations/hammer.gif", []));
         this.recognizableGestures.set("HAMMER_FAST", new GestureHammerLeapMotion("HAMMER_FAST", 1.5, "/assets/data/gestures-illustrations/hammer.gif", []));
         this.recognizableGestures.set("ROTATION", new GestureRotationLeapMotion("ROTATION", 1.5, "/assets/data/gestures-illustrations/rotation.gif", [], true));
-        this.recognizableGestures.set("REVERSED_ROTATION", new GestureRotationLeapMotion("REVERSED_ROTATION", 1.5, "/assets/data/gestures-illustrations/rotation.gif", [], false));
+        this.recognizableGestures.set("REVERSED_ROTATION", new GestureRotationLeapMotion("REVERSED_ROTATION", 1.5, "/assets/data/gestures-illustrations/reversed-rotation.gif", [], false));
     }
 
     /**
