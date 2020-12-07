91ffe40038b5666f151ac9280110e16721c2b527
Maxime ANDRE <maxime.andre@student.unamur.be>
2020-12-07 22:53:26 +0100

    Gesture pinch (blind) implementation (need to be tested with the LeapMotion)

--- js/service/GestureService.js
+++ js/service/GestureService.js
@@ -47,8 +47,8 @@ class LeapMotionGestureService extends GestureService {
         this.recognizableGestures.set("REVERSED_ROTATION", new GestureRotationLeapMotion("REVERSED_ROTATION", 1.5, "/assets/data/gestures-illustrations/reversed-rotation.gif", false));
         this.recognizableGestures.set("STAIRS", new GestureStairsLeapMotion("STAIRS", 2.8, "/assets/data/gestures-illustrations/stairs.gif"));
         this.recognizableGestures.set("SCRATCH", new GestureScratchLeapMotion("SCRATCH", 1.5, "/assets/data/gestures-illustrations/scratch.gif"));
-        this.recognizableGestures.set("PINCH", new GesturePinch1LeapMotion("PINCH", 1, "/assets/data/gestures-illustrations/pinch1.gif"));
-        this.recognizableGestures.set("PINCH", new GesturePinch3LeapMotion("PINCH3", 3, "/assets/data/gestures-illustrations/pinch3.gif"));
+        this.recognizableGestures.set("PINCH1", new GesturePinch1LeapMotion("PINCH1", 1, "/assets/data/gestures-illustrations/pinch1.gif"));
+        this.recognizableGestures.set("PINCH3", new GesturePinch3LeapMotion("PINCH3", 3, "/assets/data/gestures-illustrations/pinch3.gif"));
     }
 
     /**
